036fb7229958458ede26fd0b04cd5cc8d8065501
awnigharbia <awni2009@hotmail.com>
2018-12-03 01:34:50 +0200

    Updates

---
+++ precache-manifest.ed3d1f3ffe69495c02b403afc7173501.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7999bceacec20bd2608653031699f1f5",
+    "url": "/static/media/poster_video.7999bcea.mp4"
+  },
+  {
+    "revision": "e8b8256633fd8e3b4b2993c41b2ab9a3",
+    "url": "/static/media/popcorn.e8b82566.svg"
+  },
+  {
+    "revision": "1239a23ad32938cda96b9142d0ed53f4",
+    "url": "/static/media/logo.1239a23a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f5994c243a60fbec939",
+    "url": "/static/js/main.1f5994c2.chunk.js"
+  },
+  {
+    "revision": "e54e0ca31d91584eb579",
+    "url": "/static/js/1.e54e0ca3.chunk.js"
+  },
+  {
+    "revision": "1f5994c243a60fbec939",
+    "url": "/static/css/main.a73c7b4e.chunk.css"
+  },
+  {
+    "revision": "e54e0ca31d91584eb579",
+    "url": "/static/css/1.de4692de.chunk.css"
+  },
+  {
+    "revision": "65ccb1a36b1add7c99339b0bd3ff3f27",
+    "url": "/index.html"
+  }
+];
