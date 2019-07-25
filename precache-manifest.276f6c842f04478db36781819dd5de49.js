c370856fd80e87b2cbd06d686663317a525e3424
awnigharbia <awni2009@hotmail.com>
2019-07-25 22:53:46 +0300

    Updates

---
+++ precache-manifest.276f6c842f04478db36781819dd5de49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "21b22b18c4984544002d",
+    "url": "/filmy/static/css/main.d5a026d3.chunk.css"
+  },
+  {
+    "revision": "21b22b18c4984544002d",
+    "url": "/filmy/static/js/main.21b22b18.chunk.js"
+  },
+  {
+    "revision": "767e5453ad63c219a135",
+    "url": "/filmy/static/css/1.ee2dfe73.chunk.css"
+  },
+  {
+    "revision": "767e5453ad63c219a135",
+    "url": "/filmy/static/js/1.767e5453.chunk.js"
+  },
+  {
+    "revision": "c78d9b6a0433c88c3a67",
+    "url": "/filmy/static/js/runtime~main.c78d9b6a.js"
+  },
+  {
+    "revision": "1239a23ad32938cda96b9142d0ed53f4",
+    "url": "/filmy/static/media/logo.1239a23a.svg"
+  },
+  {
+    "revision": "7999bceacec20bd2608653031699f1f5",
+    "url": "/filmy/static/media/poster_video.7999bcea.mp4"
+  },
+  {
+    "revision": "e8b8256633fd8e3b4b2993c41b2ab9a3",
+    "url": "/filmy/static/media/popcorn.e8b82566.svg"
+  },
+  {
+    "revision": "da5c87e548506cf5912c2c7b439d9bb9",
+    "url": "/filmy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d520bdf2ad9e110ec532c42d5d14be0f.js"
+  "/filmy/precache-manifest.276f6c842f04478db36781819dd5de49.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/filmy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
