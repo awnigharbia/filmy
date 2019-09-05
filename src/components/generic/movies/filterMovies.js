7969540e2b626814e93feeb7bb9deb6b4f167344
Awni Gharbia <awni2009@hotmail.com>
2019-09-05 23:11:25 +0300

    fixing typescript branch

--- src/components/generic/movies/filterMovies.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import {Movies} from './style'
-
-export default () => (
-  <Movies.filter>
-    <Movies.filter.span>Filtered BY:</Movies.filter.span>
-  </Movies.filter>
-)
--- src/components/index.js
+++
@@ -1,1 +0,0 @@
-export {default as SidebarItem} from 'nav/sidebarItem'
